825c222ddc8f158d5c63922613ddb441739ca9fe
hoangduong04 <duongnguyen190497@gmail.com>
2017-05-12 13:02:45 +1000

    Update DB_connect.php

--- DB_connect.php
+++ DB_connect.php
@@ -1,1 +1,8 @@
+define ('DBSERVER', 'localhost:3306');
+define ('DBUSER', 'root');
+define ('DBPASS','root');
+define ('DBNAME','CSIT314');
+
+mysql_connect(DBSERVER, DBUSER, DBPASS) or die (mysql_error());
+mysql_select_database(DBNAME) or die (mysql_error());
 
