87ed867893fa5cee3d7b9b385efa235b1609ee1e
hoangduong04 <duongnguyen190497@gmail.com>
2017-05-12 13:51:39 +1000

    Update DB_connect.php

--- DB_connect.php
+++ DB_connect.php
@@ -1,8 +1,27 @@
-define ('DBSERVER', 'localhost:3306');
-define ('DBUSER', 'root');
-define ('DBPASS','root');
-define ('DBNAME','CSIT314');
+<?php
 
-mysql_connect(DBSERVER, DBUSER, DBPASS) or die (mysql_error());
-mysql_select_database(DBNAME) or die (mysql_error());
+$conn = connectDB();
 
+function connectDB() {
+  define ('DBSERVER', 'localhost:3306');
+  define ('DBUSER', 'root');
+  define ('DBPASS','root');
+  define ('DBNAME','CSIT314');
+  
+  $mysqli = new mysqli(DBSERVER, DBUSER, DBPASS, DBNAME);
+		return $mysqli;
+}
+
+function executeQuery($conn, $query, $facet) {
+  $result = $conn->query($query);
+  $i = 0;
+  $resultTable = array();
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      
+    }
+  } 
+  return $resultTable;
+}
+
+?>
